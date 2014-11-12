eb4ddb537cdb23229f247f28bd96223f3cdcce3b
Vincent Jonany <VincentJoe@d-173-250-205-191.dhcp4.washington.edu>
2014-11-12 13:02:04 -0800

    put forms

--- js/app.js
+++ js/app.js
@@ -3,3 +3,17 @@
     app.js, main Angular application script
     define your module and controllers here
 */
+
+var taskUrl = ' https://api.parse.com/1/classes/tasks';
+
+angular.module('RateApp', [])
+	.config(function($httpProvider) {
+		$httpProvider.default.headers.common['X-Parse-Application-Id'] = 'wSTIwnlIBAEYc8sj1BYfr1Pl3WfLoSiYXqDRph03';
+		$httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = '7u7h7u5mEtcuLTgcGa7POurLgtJ9IAftfoluXnZX';
+	}).controller('TasksController', function($scope, $http) {
+
+
+
+
+
+	});
