21ada799a1845cad5091b4b302b100b4599e729f
vincent <jonanv@uw.edu>
2014-11-12 20:07:07 -0800

    needs more

--- js/app.js
+++ js/app.js
@@ -4,7 +4,50 @@
     define your module and controllers here
 */
 
-var taskUrl = ' https://api.parse.com/1/classes/tasks';
+var taskUrl = 'https://api.parse.com/1/classes/comments';
+
+document.getElementById("comment-form").addEventListener("submit", onSubmit);
+
+function onSubmit(evt) {
+    evt.returnValue = validateForm(this);
+
+    if (evt.returnValue == false && evt.preventDefault) {
+        evt.preventDefault();
+        document.getElementsByClassName("btn")[0].style.display = "none";
+    }
+
+    return evt.returnValue;
+} //onSubmit()
+
+
+function validateForm(form) {
+    var requiredFields = ['name', 'comment', 'title'];
+    var formValid = true;
+
+    for(var i = 0; i < requiredFields.length; i++) {
+        formValid &= validateRequiredField(form.elements[requiredFields[i]]);
+    }
+
+    return formValid;
+} //validateForm()
+
+
+function validateRequiredField(field) {
+    var value = field.value.trim();
+    var valid = value.length > 0;
+    if(valid) {
+        field.className = "form-control";
+
+    }
+
+    else {
+        field.className = "form-control invalid-field";
+    }
+
+    return valid;
+
+
+}
 
 angular.module('RateApp', ['ui.bootstrap'])
 	.config(function($httpProvider) {
@@ -13,11 +56,9 @@ angular.module('RateApp', ['ui.bootstrap'])
 	}).controller('CommentsController', function($scope, $http) {
 
 
-//        $scope.isEmpty = true;
-
         $scope.refreshComments = function() {
             $scope.loading = true;
-            $http.get(taskUrl).success(function (data) {
+            $http.get(taskUrl).success(function(data) {
                 $scope.comments = data.results;
 
                 if(data.results.length == 0) {
@@ -39,30 +80,36 @@ angular.module('RateApp', ['ui.bootstrap'])
         };
 
         $scope.refreshComments();
-        $scope.newComments = '';
+
+        $scope.newComments = {
+
+            title: '',
+            name: '',
+            comment: '',
+            ratings: null
+
+
+
+        };
+
 
         $scope.addComments = function() {
             $http.post(taskUrl, $scope.newComments).success(function(responseData) {
                 $scope.newComments.objectId = responseData.objectId;
+
+
                 $scope.comments.push($scope.newComments);
                 $scope.isEmpty = false;
             }).error(function(err) {
                 $scope.errorMessage = err;
 
+            }).finally(function() {
+
+                $scope.newComments = {};
             });
 
 
         };
-
-//        $scope.updateTask = function(comment) {
-//
-//            $http.put(taskUrl + '/' + comment.objectId, comment).success(function() {
-//
-//            }).error(function(err) {
-//                $scope.errorMessage = err;
-//            });
-//        };
-
 
 
 
